9db57541a47a659013aaf526697b097c94ba8270
Robert Sageme <52455548+robomano-glitch@users.noreply.github.com>
2023-09-13 21:50:53 +0200

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <nav>
-    <div class="flex-gap-2">
-      <h1>Rob<span class="text-secondary">Sageme</span></h1>
-      <img> 
-    </div>
-  </nav>
+  <div class="container overflow-x-hidden">
+    <Navbar />
+    <Home />
+    <TechStack />
+    <Projects />
+    <WorkExperience />
+    <Footer />
+  </div>
 </template>
+
+<script setup>
+  import Navbar from './components/MyNavbar.vue';
+  import Home from './components/MyHome.vue';
+  import TechStack from './components/TechStack.vue';
+  import Projects from './components/MyProjects.vue';
+  import WorkExperience from './components/WorkExperience.vue';
+  import Footer from './components/MyFooter.vue';
+</script>   
 
 <style>
 
---
+++ src/components/MyHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <section>
+        <div>
+            <div>
+                <div class="flex items-ccenter gap-3">
+                    <h2>Hello</h2>
+                </div>
+                <h2 class="text-4xl md:text-5xl">Welcome</h2>
+                <p class="pt-5 text-lg font-normal">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae eius quibusdam eveniet possimus voluptatem odio iure alias exercitationem enim blanditiis iusto, laudantium numquam saepe et commodi magnam ratione quis? Vero. </p>   
+            </div>
+        </div>
+    </section>
+</template>
+  
+<script setup>
+</script>   
+  
+<style>    
+</style>
+  
